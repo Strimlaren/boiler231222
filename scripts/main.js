999635937c4fc51ff73e78a1245da943b123d03e
Mirza Mesinovic <mirza.mesinovic@chasacademy.com>
2023-12-22 11:32:23 +0100

    finished hidden card

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,20 @@
-const luckor = document.querySelectorAll(".lucka");
+const submit = document.querySelector(".submit");
+const dialog = document.querySelector(".dialog");
 
-function dialog() {
-  const dialog = document.createElement("dialog");
-  const form = document.createElement;
-}
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  dialog.setAttribute("open", "true");
+});
+
+const close_button = document.getElementById("close-dialog");
+
+close_button.addEventListener("click", () => {
+  dialog.close();
+});
+
+const our_form = document.querySelector("form");
+const lucka24 = document.getElementById("24");
+
+lucka24.addEventListener("click", () => {
+  our_form.classList.add("show-form");
+});
